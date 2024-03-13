54932e1b1e8a7d5e998cc67f6cede9cc282295b3
Santiago Osorio <saostero@gmail.com>
2024-03-13 16:43:14 -0500

    useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { MemoHook } from "./06-memos/MemoHook";
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
-import { SimpleForm } from "./02-useEffect/SimpleForm";
-import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
-import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
-import { FocusScreen } from "./04-useRef/FocusScreen";
-import { Layout } from "./05-useLayoutEffect/Layout";
-import { Memorize } from "./06-memos/Memorize";
+// import { SimpleForm } from "./02-useEffect/SimpleForm";
+// import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { Layout } from "./05-useLayoutEffect/Layout";
+// import { Memorize } from "./06-memos/Memorize";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
@@ -22,7 +23,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     {/* <MultipleCustomHooks/> */}
     {/* <FocusScreen/> */}
     {/* <Layout/> */}
-    <Memorize/>
+    {/* <Memorize/> */}
+    <MemoHook/>
   </>
   // </React.StrictMode>
 );
