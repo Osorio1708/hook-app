fa9b362ee09a0cdc54429320eeeaaf971d315045
Santiago Osorio <saostero@gmail.com>
2024-03-14 14:09:30 -0500

    Tarea Memo

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import { MemoHook } from "./06-memos/MemoHook";
 import { CallbackHook } from "./06-memos/CallbackHook";
+import { Padre } from "./07-tarea-memo/Padre";
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
@@ -26,7 +27,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     {/* <Layout/> */}
     {/* <Memorize/> */}
     {/* <MemoHook/> */}
-    <CallbackHook/>
+    {/* <CallbackHook/> */}
+    <Padre/>
   </>
   // </React.StrictMode>
 );
