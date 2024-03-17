a6f9f4b3e2b7af739a75bfb1127fcf283ce60282
Santiago Osorio <saostero@gmail.com>
2024-03-17 03:39:47 -0500

    Toggle Todo

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -41,7 +41,15 @@ export const TodoApp = () => {
     };
     dispatch(action);
   };
-  
+
+  const handleToggleTodo = (id) => {
+    const action = {
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
   return (
     <>
       <h1>
@@ -50,7 +58,11 @@ export const TodoApp = () => {
       <hr />
       <div className="row">
         <div className="col-7">
-          <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
+          <TodoList
+            todos={todos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
         </div>
         <div className="col-5">
           <h4>Agregar TODO</h4>
--- src/08-useReducer/TodoItem.jsx
+++ src/08-useReducer/TodoItem.jsx
@@ -1,9 +1,16 @@
-export const TodoItem = ({ todo, onDeleteTodo }) => {
+export const TodoItem = ({ todo, onDeleteTodo, onToggleTodo }) => {
   const { id, description } = todo;
   return (
     <>
       <li className="list-group-item d-flex justify-content-between">
-        <span className="aling-self-center">{description}</span>
+        <span
+          className={`aling-self-center ${
+            todo.done && "text-decoration-line-through"
+          }`}
+          onClick={() => onToggleTodo(id)}
+        >
+          {description}
+        </span>
         <button className="btn btn-danger" onClick={() => onDeleteTodo(id)}>
           borrar
         </button>
