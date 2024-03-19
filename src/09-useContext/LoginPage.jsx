72c4406e27e263f39e2334d63171815de66f8699
Santiago Osorio <saostero@gmail.com>
2024-03-19 11:57:48 -0500

    useContext

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,8 +1,59 @@
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
+
 export const LoginPage = () => {
+  const { user, setUser } = useContext(UserContext);
+  const { id, name, email } = user || { id: "", name: "", email: "" };
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
   return (
     <>
       <h1>LoginPage</h1>
       <hr />
+      <pre>{JSON.stringify(user, null, 3)}</pre>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <input
+          className="form-control mt-2"
+          type="text"
+          name="id"
+          placeholder="User id"
+          value={id}
+          onChange={onInputChange}
+        />
+        <input
+          className="form-control mt-2"
+          type="text"
+          name="name"
+          placeholder="User Name"
+          value={name}
+          onChange={onInputChange}
+        />
+        <input
+          className="form-control mt-2"
+          type="enail"
+          name="email"
+          placeholder="User Email"
+          value={email}
+          onChange={onInputChange}
+        />
+      </form>
+      <button
+        className="btn btn-primary mt-2"
+        onClick={() => {
+          setUser({ id: "", name: "", email: "" });
+        }}
+      >
+        Clear
+      </button>
     </>
   );
 };
--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -3,12 +3,13 @@ import { HomePage } from "./HomePage";
 import { LoginPage } from "./LoginPage";
 import { AboutPage } from "./AboutPage";
 import { Navbar } from "./Navbar";
+import { UserProvider } from "./context/UserProvider";
 
 export const MainApp = () => {
   return (
-    <>
+    <UserProvider>
       <h1>MainApp</h1>
-      <Navbar/>
+      <Navbar />
       <hr />
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -16,6 +17,6 @@ export const MainApp = () => {
         <Route path="about" element={<AboutPage />} />
         <Route path="/*" element={<Navigate to="/about" />} />
       </Routes>
-    </>
+    </UserProvider>
   );
 };
